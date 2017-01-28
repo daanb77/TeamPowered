04c963f2088cdfe5e9c1fb0333cefba3c1329280
W1-BroekhuizenD <daan.broekhuizen@edu-kw1c.nl>
2017-01-28 15:32:12 +0100

    Sagbi

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -8,7 +8,8 @@ namespace TeamPowered
 {
     class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
         public string Adress { get; set; }
     }
 }
